59ddde36a55b25890a61294e90b956d55c106d4e
Leny Bressoud <enderastronaute@gmail.com>
2026-05-08 10:21:29 +0200

    Seed location change

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo "Running migrations..."
 uv run alembic upgrade head
 
 echo "Seeding database..."
-uv run python seed/seed.py
+uv run python seed.py
 
 echo "Starting app..."
 exec uv run streamlit run main.py
